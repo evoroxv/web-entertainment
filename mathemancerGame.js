548a4d3543a7060f0330736e6a6de9748fafab83
evoroxv <evo3xv@gmail.com>
2018-08-07 23:13:21 +0100

    altered button positioning and shifted scripts
    Scripts are now located in the mathemancerGame.js file, and the spell buttons have been reordered somewhat.

---
+++ mathemancerGame.js
@@ -0,0 +1,16 @@
+function startGame() {
+  myGameArea.start();
+  }
+
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 800;
+    this.canvas.height = 600;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  }
+}
+function castSpell() {
+
+}
