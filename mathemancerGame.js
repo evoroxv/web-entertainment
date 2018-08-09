4ebbc821b32df95d4e1e641d09f4b5b2f565eb2a
evoroxv <evo3xv@gmail.com>
2018-08-09 22:43:06 +0100

    added content
    Added Goblin monster, still needs to be finished. Also added levelUp functions and reorganised code

--- mathemancerGame.js
+++ mathemancerGame.js
@@ -1,22 +1,6 @@
-var experience = 0
-var monsters_slain = 0
-var level = 1
-var levelLimit = {
-  (level**2)*10;
-}
-
-var health = {
-  level*5;
-}
-
-function levelUp() {
-  level = (level+1);
-}
-
 function startGame() {
   myGameArea.start();
-  }
-
+}
 var myGameArea = {
   canvas : document.createElement("canvas"),
   start : function() {
@@ -26,13 +10,39 @@ var myGameArea = {
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
   }
 }
-function castSpell() {
-
+var toHit = Math.floor(Math.random() * 100) + 1;
+var experience = 0;
+var monsters_slain = 0;
+var level = 1;
+var levelLimit = {
+  (level**2)*10;
 }
-
+var health = {
+  level*5;
+}
+function levelUp() {
+  level = (level+1);
+}
 function gainXP() {
   experience = (experience + monsterXp)
   if (experience >= levelLimit) {
     levelUp();
+  }
+}
+function castSpell() {
+
+}
+var goblin = {
+  name : "Goblin",
+  id : "goblin",
+  hitPoints : 5,
+  damage : 1,
+  attack : dealDamage() {
+    if (toHit <= 50) {
+      health = (health - goblin.damage)
+    }
+    else {
+      
+    }
   }
 }
